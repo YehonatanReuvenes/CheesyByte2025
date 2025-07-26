b2e6b48a04643b8ad816515c3d69e78f3a85a62d
jonat <yehonatan.reuvenes@gmail.com>
2025-07-26 20:24:55 +0300

    adding drive method

--- CheesyByte2025.ino
+++ CheesyByte2025.ino
@@ -1,3 +1,5 @@
+#include <utility>
+
 const int pwmInPinWepond = 1;
 const int pwmInPinDrive_x = 1;
 const int pwmInPinDrive_y = 1;
@@ -15,13 +17,28 @@ void setup(){
 }
 
 void loop(){
-    pwmWepond = pulseIn(pwmInPinWepond, HIGH);
-    pwmDrive_x = pulseIn(pwmInPinDrive_x, HIGH);
-    pwmDrive_y = pulseIn(pwmInPinDrive_y, HIGH);
+    int pwmWepond = pulseIn(pwmInPinWepond, HIGH);
+    int pwmDrive_x = pulseIn(pwmInPinDrive_x, HIGH);
+    int pwmDrive_y = pulseIn(pwmInPinDrive_y, HIGH);
 
     int outWepond, outRightWheel, outLeftWheel;
+
+    std::pair<int, int> driveOut = tankDriveOutputs(pwmDrive_x,pwmDrive_y);
+    outRightWheel = driveOut.first;
+    outLeftWheel = driveOut.second;
+    
 
     analogWrite(pwmOutPinWepond, outWepond);
     analogWrite(pwmOutPinRightWheel, outRightWheel);
     analogWrite(pwmOutPinLeftWheel, outLeftWheel);
+}
+
+std::pair<int, int> tankDriveOutputs(int x, int y) {
+    x = 2 * (x - 127)
+    y = 2 * max(0, y - 127);
+
+    int right = min(max(0, y - x), 255);
+    int left = min(max(0, y + x), 255);
+
+    return {right, left};
 }
